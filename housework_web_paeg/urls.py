0fca0e8ae84c278cf032972cfe2227bb559c74c9
Davor Telisman <davortelisman@gmail.com>
2022-08-29 09:12:24 +0200

    Displaying user profile image to are page: Add in settings: MEDIA_ROOT = os.path.join(BASE_DIR, 'media') STATIC_DIR = os.path.join(BASE_DIR, 'static')
    Next:
    Go to housework_web_paeg/urls.py and add:
    from django.conf.urls.static import static
    from django.conf import settings
    urlpatterns = []+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) to get image from media file

--- housework_web_paeg/urls.py
+++ housework_web_paeg/urls.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.urls import re_path as url
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('home_page.urls')),
-    path('clients/', include('django.contrib.auth.urls')),
-    path('clients/', include('clients.urls')),
-    path('chores/', include('django.contrib.auth.urls')),
-    path('chores/', include('chores.urls')),
-    url(r'^messages/', include('django_messages.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
-]
+                  path('admin/', admin.site.urls),
+                  path('', include('home_page.urls')),
+                  path('clients/', include('django.contrib.auth.urls')),
+                  path('clients/', include('clients.urls')),
+                  path('chores/', include('django.contrib.auth.urls')),
+                  path('chores/', include('chores.urls')),
+                  url(r'^messages/', include('django_messages.urls')),
+                  path("__reload__/", include("django_browser_reload.urls")),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
